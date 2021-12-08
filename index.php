6b0cdf2c2501fd10431e3c9dbf5fefe6bc85d221
Kinnari Nilesh Patil <86309276+kinnaripatil04@users.noreply.github.com>
2021-12-08 21:28:30 +0530

    dfp files

---
+++ index.php
@@ -0,0 +1,222 @@
+<!doctype html>
+<html>
+  <head>
+    <title>Contact Us</title>
+    <!--Bootstrap Link-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+    <!----------------- BOOTSTRAP CSS ------------------->
+    <link rel="stylesheet" href="bootstrap.min.css">
+
+    <!------------- FONT AWESOME CDN LINK --------------->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.2/css/all.min.css">
+    
+    <!------------------ MAIN STYLE CSS ----------------->
+    <link rel="stylesheet" href="style.css">
+
+    <!--Counter Links-->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/waypoints/4.0.1/jquery.waypoints.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Counter-Up/1.0.0/jquery.counterup.min.js"></script>
+
+  </head>
+  <body>
+    
+    <!---------------------- ABOUT US SECTION STARTS ----------------------
+     -------------------------------------------------------------------------------->
+    <section id="about-wrapper">
+      <div class="mission-container container">
+          <div class="row">
+              <div class="col-lg-8 mr-space">
+                  <div class="mission-hr-line">
+                      <h3>About DFP</h3>
+                  </div>
+                  <div class="mission-subtitle">
+                    <br>
+                      <p class="mb-5" style="color:#8C8C9E">Lorem ipsum dolor sit amet, consectetur adipiscing elit
+                        . Aenean blandit egestas elit ac ultricies. Donec nec rhoncus lectus. Praesent sagittis tincid
+                        augue facilisis feugiat. Aliquam vitae augue et tellus molestie gravida. Fusce nec tellus neque. 
+                        Aliquam non porttitor felis. Integer id ligula luctus nulla mollis elementum nec eu metus. Aliqu
+                        am facilisis arcu nunc, et sodales nisi malesuada vitae. Integer sollicitudin ipsum eget nulla max
+                        imus ultricies. Phasellus at luctus augue, et scelerisque erat. Nullam tincidunt lectus sed dolor 
+                        maximus, ac sodales sapien aliquet.
+                      </p>
+                  </div>
+                  <div class="about-nut">
+                    <section id="service-main-wrapper">
+                      <div class="service-wrapper">
+                          <div class="container service-container">
+                              <div class="row service-table-wrap">
+                                  <div class="col-md-6 col-lg-4 card-deck">
+                                      <div class="card">
+                                          <div class="card-logo">
+                                              <img src="vision.png" alt="">
+                                          </div>
+                                          <div class="card-body">
+                                              <h4 class="card-title">
+                                                  Vision
+                                              </h4>
+                                              <div class="service-list-details">
+                                                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit
+                                                    . Aenean blandit egestas elit ac ultricies.</p>
+                                              </div>
+                                          </div>
+                                      </div>
+                                  </div>
+                                  <div class="col-md-6 col-lg-4 card-deck">
+                                      <div class="card">
+                                          <div class="card-logo">
+                                              <img src="mission.png" alt="">
+                                          </div>
+                                          <div class="card-body">
+                                              <h4 class="card-title">
+                                                Mission
+                                              </h4>
+                                              <div class="service-list-details">
+                                                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit
+                                                    . Aenean blandit egestas elit ac ultricies.</p>
+                                              </div>
+                                            </div>
+                                      </div>
+                                  </div>
+                                  <div class="col-md-6 col-lg-4 card-deck">
+                                      <div class="card">
+                                          <div class="card-logo">
+                                              <img src="team.png" alt="">
+                                          </div>
+                                          <div class="card-body">
+                                              <h4 class="card-title">
+                                                  Team
+                                              </h4>
+                                              <div class="service-list-details">
+                                                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit
+                                                    . Aenean blandit egestas elit ac ultricies.</p>
+                                              </div>
+                                          </div>
+                                      </div>
+                                  </div>
+                              </div>
+                          </div>
+                      </div>
+                  </section>
+                  </div>
+              </div>
+              <div class="col-lg-4">
+                  <div class="grid-gallery">
+                    <div class="gallery-img-a">
+                      <div class="card" style="width: 25rem;">
+                        <img src="founder.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                          <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit
+                            . Aenean blandit egestas elit ac ultricies. Donec nec rhoncus lectus. Praesent sagittis tincid
+                            augue facilisis feugiat. Aliquam vitae augue et tellus molestie gravida. Fusce nec tellus neque. 
+                            Aliquam non porttitor felis. Integer id ligula luctus nulla mollis elementum nec eu metus. Aliqu
+                            am facilisis arcu nunc, et sodales nisi malesuada vitae. Integer sollicitudin ipsum eget nulla max
+                            imus ultricies. Phasellus at luctus augue, et scelerisque erat. Nullam tincidunt lectus sed dolor 
+                            maximus, ac sodales sapien aliquet. Lorem ipsum dolor sit amet, consectetur adipiscing elit
+                            . Aenean blandit egestas elit ac ultricies. 
+                          </p></p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+              </div>
+          </div>
+      </div>
+      <!--Counter Code-->
+      <div class="counter-up">
+        <div class="content">
+          <div class="box">
+            <div class="icon"><i class="fas fa-users-cog"></i></div>
+            <div class="counter">724</div>
+            <div class="text">Staff</div>
+          </div>
+          <div class="box">
+            <div class="icon"><i class="fas fa-paper-plane"></i></div>
+            <div class="counter">67508</div>
+            <div class="text">Social Media Followers</div>
+          </div>
+          <div class="box">
+            <div class="icon"><i class="fas fa-users"></i></div>
+            <div class="counter">2436</div>
+            <div class="text">Happy Customers</div>
+          </div>
+          <div class="box">
+            <div class="icon"><i class="fas fa-times"></i></div>
+            <div class="counter">0</div>
+            <div class="text">Complaints</div>
+          </div>
+        </div>
+      </div>
+      <script>
+        $(document).ready(function(){
+          $('.counter').counterUp({
+            delay: 10,
+            time: 1200
+          });
+        });
+        </script>
+      
+      <!--Counter Code-->
+    </section>
+    <!---------------------- ABOUT US SECTION ENDS -----------------------
+    -------------------------------------------------------------------------------->
+
+
+    <!--Contact us starts-->
+    <section id="about-wrapper">
+      <div class="mission-container container">
+          <div class="row">
+              <div class="col-lg-4 mr-space">
+                  <div class="mission-hr-line">
+                      <h3>Contact Us</h3>
+                  </div>
+                  <div class="about-nut">
+                      <div class="about-list">
+                        <div class="grid-gallery">
+                          <br>
+                          <br>
+                          <div class="gallery-img-a"><img src="contactusgif.gif" alt=""></div>
+                        </div>
+                      </div>
+                  </div>
+              </div>
+              <div class="col-lg-8">
+                <form method="post" action="contact.php">
+                  <br>
+                  <fieldset>
+                    <div class="form-row">
+                      <div class="col">
+                        <label for="exampleFormControlInput1">First Name</label>
+                        <input type="text" class="form-control" placeholder="First name" name="first_name">
+                      </div>
+                      <div class="col">
+                        <label for="exampleFormControlInput1">Last Name</label>
+                        <input type="text" class="form-control" placeholder="Last name" name="last_name">
+                      </div>
+                    </div>
+                    <br>
+                    <div class="form-row">
+                      <label for="exampleFormControlInput1">Email address</label>
+                      <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com" name="email">
+                    </div>
+                    <br>
+                    <div class="form-row">
+                      <label for="exampleFormControlInput1">Phone No</label>
+                      <input type="tel" class="form-control" id="exampleFormControlInput1" placeholder="9000800010" name="phone">
+                    </div>
+                    <br>
+                    <div class="form-group">
+                      <label for="exampleFormControlTextarea1">Reason for Contact</label>
+                      <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" name="reason"></textarea>
+                    </div>
+                    <button type="submit" class="buttons" value="submit">Submit</button>
+                  </fieldset>
+                </form>
+              </div>
+          </div>
+      </div>
+  </section>
+  <a href="registration/payment.html">Payment</a>
+    <!--Contact Us ends-->
+  </body>
+</html>
---
+++ login/config.php
@@ -0,0 +1,12 @@
+<?php 
+
+$server = "localhost";
+$user = "root";
+$pass = "";
+$database = "login";
+
+$conn = mysqli_connect($server, $user, $pass, $database);
+
+
+
+?>
